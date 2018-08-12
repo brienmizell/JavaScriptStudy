047679491d0e3f24c1129691d5584cf6344b7356
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2018-08-12 16:54:21 -0400

    finished lecture 22

--- script.js
+++ script.js
@@ -153,7 +153,7 @@ console.log(firstName + ' is a teenager.')
     } else {
     console.log(firstName + ' is a man.')
 }
-*/
+
 
 var firstName = 'John';
 var age = 16;
@@ -261,7 +261,7 @@ function yearsUntilRetirement(birthYear, firstName) {
 
 yearsUntilRetirement(1981, 'Brien');
 yearsUntilRetirement(1979, 'Brooks')
-*/
+
 
 // function declaration
 // function whatDoYouDo(job, firstName) {}
@@ -282,3 +282,28 @@ var whatDoYouDo = function(job, firstName) {
 console.log(whatDoYouDo('teacher', 'John'));
 console.log(whatDoYouDo('designer', 'Jane'));
 console.log(whatDoYouDo('retired', 'Mark'));
+*/
+
+var names = ['John', 'Mark', 'Jane'];
+var years = new Array(1990, 1969, 1948);
+
+console.log (names[2]);
+console.log(names.length);
+
+names[1] = 'Ben';
+names[names.length] = 'Mary';
+console.log(names);
+
+var john = ['John', 'Smith', 1990, 'teacher', false];
+
+john.push('blue');
+john.unshift('Mr.')
+john.pop();
+john.pop();
+john.shift();
+console.log(john);
+
+console.log(john.indexOf(1990));
+
+var isDesigner = john.indexOf('designer') === -1 ? 'John is NOT a designer' : 'John is a designer.';
+console.log(isDesigner);
