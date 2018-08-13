cc8427b3aa755456d6094636f5dbf27ee2ea186d
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2018-08-12 22:03:00 -0400

    finished lecture 25

--- script.js
+++ script.js
@@ -354,3 +354,18 @@ jane.birthYear = 1969;
 jane['lastName'] = 'Smith';
 console.log(jane);
 */
+
+var john = {
+    firstName: 'John',
+    lastName: 'Smith',
+    birthYear: 1992,
+    family: ['Jane', 'Mark', 'Bob', 'Emily'],
+    job: 'teacher',
+    isMarried: false,
+    calcAge: function() {
+        this.age = 2018 - this.birthYear;
+    }
+};
+
+john.calcAge();
+console.log(john);
